ddc6dd95f3df7ee0fddb3d22f2bba772384e6bd5
Ozan <ozank2001@gmail.com>
2026-02-19 16:25:54 +0300

    feat: add weight object model and random color generator

--- script.js
+++ script.js
@@ -5,9 +5,25 @@ const state = {
   logs: []
 };
 
-const randomWeightGenerator = () => {
-    return Math.floor(Math.random() * 10) + 1;
+const getRandomWeight = () => {
+    let weight = Math.floor(Math.random() * 10) + 1;
+    console.log(weight);
+    return weight;
 }
 
-let weight = randomWeightGenerator();
+const getRandomColor = () => {
+  const colors = ["#E74C3C", "#3598DB", "#F39C12", "#34495E", "#2FCC71", "#9B59B6"];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+const createWeight = (xPosition) => {
+  return {
+    weight: getRandomWeight(),
+    color: getRandomColor(),
+    x: xPosition,
+    y: 0
+  };
+}
+
+weight = createWeight(200);
 console.log(weight);
